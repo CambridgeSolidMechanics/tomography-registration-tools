fdb5e2e3864a1c51da3de47e13b1d7a752fbcda1
Ivan Grega <grega.ivan@gmail.com>
2024-03-12 10:43:26 +0000

    Rearranged order of b-spline dof and added documentation

--- t_core/bsplinefield.py
+++ t_core/bsplinefield.py
@@ -27,8 +27,24 @@ def load_bspline_params(path: Union[str, Path], units_multiplier: float=1.0) -> 
     return out
 
 class BSplineField(DisplacementField):
+    """Cubic B-spline 3d field.
+    """
+    
     @staticmethod
-    def bspline(u, i):
+    @lru_cache
+    def bspline(u: torch.Tensor, i: int) -> torch.Tensor:
+        """B-spline functions.
+
+        Using lru_cache to speed up the computation if 
+        the same u and i are used multiple times.
+
+        Args:
+            u (torch.Tensor): coordinate in domain
+            i (int): index of the B-spline. One of {0,1,2,3}
+
+        Returns:
+            torch.Tensor: B-spline weight
+        """
         if i == 0:
             return (1 - u)**3 / 6
         elif i == 1:
@@ -38,48 +54,116 @@ class BSplineField(DisplacementField):
         elif i == 3:
             return u**3 / 6
 
-    def __init__(self, phi_x: np.ndarray, origin=(-1,-1,-1), spacing=(1,1,1)) -> None:
+    def __init__(
+            self, 
+            phi_x: Union[torch.Tensor, np.ndarray], 
+            origin=(-1,-1,-1), 
+            spacing=(1,1,1),
+            **kwargs
+    ) -> None:
+        """Set up the B-spline field.
+
+        Args:
+            phi_x (Union[torch.Tensor, np.ndarray]): degrees of freedom 
+                of the B-spline field in order [dim, nx, ny, nz]
+            origin (tuple, optional): coordinates of the first control point. 
+                Defaults to (-1,-1,-1).
+            spacing (tuple, optional): spacing between control points 
+                along each dimension. Defaults to (1,1,1).
+        """
         super().__init__()
+        if 'class' in kwargs:
+            assert kwargs['class'] == 'BSplineField'
         assert phi_x.ndim == 4
+        if isinstance(phi_x, np.ndarray):
+            phi_x = torch.tensor(phi_x, dtype=torch.float32)
         self.phi_x = phi_x
-        _,nz,ny,nx = phi_x.shape
+        _,nx,ny,nz = phi_x.shape
         self.grid_size = (nx, ny, nz)
         self.origin = origin
         self.spacing = spacing
 
 
-    def displacement(self, x: torch.Tensor, y: torch.Tensor, z: torch.Tensor, i: int, **kwargs):
+    def displacement(
+            self, 
+            x: torch.Tensor, y: torch.Tensor, z: torch.Tensor, 
+            i: int, 
+            **kwargs
+    ) -> torch.Tensor:
+        """Displacement at points x,y,z in the direction i.
+
+        We implement support for locations beyond control points.
+
+        Args:
+            x (torch.Tensor): x-coordinates. Can be 1d or meshgrid.
+            y (torch.Tensor): y-coordinates. -"-
+            z (torch.Tensor): z-coordinates. -"-
+            i (int): index of the displacement direction. (x=0, y=1, z=2)
+
+        Returns:
+            torch.Tensor: displacement
+        """
         dx, dy, dz = self.spacing
         u = (x - self.origin[0] - dx)/dx
         v = (y - self.origin[1] - dy)/dy
         w = (z - self.origin[2] - dz)/dz
-        ix = np.floor(u).astype(int)
-        iy = np.floor(v).astype(int)
-        iz = np.floor(w).astype(int)
+        ix = torch.floor(u).long()
+        iy = torch.floor(v).long()
+        iz = torch.floor(w).long()
         u = u - ix
         v = v - iy
         w = w - iz
-        T = np.zeros_like(x)
+        T = torch.zeros_like(x, dtype=torch.float32)
         for l in range(4):
-            ix_loc = np.clip(ix + l, 0, self.grid_size[0]-1)
+            ix_loc = torch.clamp(ix + l, 0, self.grid_size[0]-1)
             for m in range(4):
-                iy_loc = np.clip(iy + m, 0, self.grid_size[1]-1)
+                iy_loc = torch.clamp(iy + m, 0, self.grid_size[1]-1)
                 for n in range(4):
-                    iz_loc = np.clip(iz + n, 0, self.grid_size[2]-1)
-                    T += self.bspline(u, l) * self.bspline(v, m) * self.bspline(w, n) * self.phi_x[i, iz_loc, iy_loc, ix_loc]
+                    iz_loc = torch.clamp(iz + n, 0, self.grid_size[2]-1)
+                    T += self.bspline(u, l) * self.bspline(v, m) * self.bspline(w, n) * self.phi_x[i, ix_loc, iy_loc, iz_loc]
         return T
     
     @staticmethod
-    def from_transform_file(path: Union[str, Path], units_multiplier: float = 1.0):
+    def from_transform_file(
+        path: Union[str, Path], units_multiplier: float = 1.0
+    ) -> "BSplineField":
+        """Load B-spline field from transform file.
+
+        The transform file is deemed to follow the transformix convention.
+        Importantly, the degrees of freedom of the b-splines are saved in
+        order: [dim, nz, ny, nx]. We convert to order [dim, nx, ny, nz].
+
+        Args:
+            path (Union[str, Path]): path to transform file
+            units_multiplier (float, optional): number by which to scale 
+                length units - e.g. to go from mm to um, use 1000.
+                Defaults to 1.0.
+
+        Returns:
+            BSplineField
+        """
         params = load_bspline_params(path, units_multiplier)
         nx, ny, nz = params['grid_size']
         phi_x = np.array(params['transform_params']).reshape(3, nz, ny, nx)
+        phi_x = phi_x.swapaxes(1,3)
         return BSplineField(phi_x, params['origin'], params['spacing'])
     
     @staticmethod
-    def from_dict(d: Dict):
+    def from_dict(d: Dict) -> "BSplineField":
+        """Load field from dictionary.
+
+        Our convention is to save b-spline dof in order [dim, nx, ny, nz].
+
+        Args:
+            d (Dict): dictionary with field parameters
+
+        Returns:
+            BSplineField
+        """
         phi_x = np.array(d['phi_x']).reshape(3, *d['grid_size'])
-        return BSplineField(phi_x, d['origin'], d['spacing'])
+        kwargs = {key:val for key,val in d.items() if key not in ['phi_x']}
+        kwargs['phi_x'] = phi_x
+        return BSplineField(**kwargs)
     
     def to_dict(self) -> Dict:
         return {
